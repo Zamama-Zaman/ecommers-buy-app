189b0cf94e66d1b47f62c2062ca987033757d1a5
zamama-zaman <zamamazaman@gmail.com>
2022-08-09 23:01:33 +0500

    Updated Code

---
+++ lib/presentation/pages/login_page_two.dart
@@ -0,0 +1,146 @@
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
+
+import 'package:flutter/material.dart';
+import 'package:ugks_limousine_app/presentation/pages/default_page.dart';
+
+import '../../size_config.dart';
+
+class LoginPageTwo extends StatelessWidget {
+  const LoginPageTwo({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    double screenHeight = MediaQuery.of(context).size.height;
+    SizeConfig().init(context);
+    return Scaffold(
+      backgroundColor: Color(0xFF201B19),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 30.0),
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              SizedBox(height: screenHeight / 15),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                child: Image.asset(
+                  "assets/images/logo.png",
+                  // height: 50,
+                  // width: 150,
+                ),
+              ),
+              SizedBox(height: 20),
+              Container(
+                height: 56,
+                width: 330,
+                alignment: Alignment.center,
+                child: Text(
+                  "Enter your email to log in or create an account",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 25,
+                    fontWeight: FontWeight.w700,
+                  ),
+                ),
+              ),
+              SizedBox(height: 20),
+              Container(
+                width: 330,
+                padding: EdgeInsets.symmetric(horizontal: 10),
+                decoration: BoxDecoration(
+                  color: Colors.transparent,
+                  border: Border.all(
+                    color: Colors.white,
+                    width: 1,
+                  ),
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    SizedBox(height: 8),
+                    Text(
+                      "Email",
+                      style: TextStyle(
+                        color: Colors.white,
+                      ),
+                    ),
+                    Container(
+                      height: 25,
+                      width: double.infinity,
+                      color: Colors.transparent,
+                      child: TextFormField(
+                        keyboardType: TextInputType.emailAddress,
+                        style: TextStyle(
+                          color: Colors.white,
+                        ),
+                        decoration: InputDecoration(
+                          border: InputBorder.none,
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: 5),
+                  ],
+                ),
+              ),
+              SizedBox(height: 20),
+              InkWell(
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => DefaultPage(),
+                    ),
+                  );
+                },
+                child: Container(
+                  width: 330,
+                  height: 56,
+                  padding: EdgeInsets.symmetric(horizontal: 10),
+                  decoration: BoxDecoration(
+                    color: Color(0xFFF24A40),
+                  ),
+                  alignment: Alignment.center,
+                  child: Text(
+                    "Continue",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 20,
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(height: 20),
+              Container(
+                alignment: Alignment.center,
+                child: Text(
+                  "or continue with",
+                  style: TextStyle(
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+              SizedBox(height: 20),
+              Container(
+                width: 330,
+                height: 56,
+                padding: EdgeInsets.symmetric(horizontal: 10),
+                decoration: BoxDecoration(
+                  border: Border.all(
+                    color: Color(0xFF1877F2),
+                    width: 1,
+                  ),
+                ),
+                alignment: Alignment.center,
+                child: Icon(
+                  Icons.facebook,
+                  color: Color(0xFF1877F2),
+                  size: 30,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
